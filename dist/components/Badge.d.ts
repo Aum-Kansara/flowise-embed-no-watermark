69132fba78edb0168da24ef0b903c96d03e2b0e4
Aum <aumkan23@gmail.com>
2023-10-25 13:06:52 +0530

    Initial Commir

---
+++ dist/components/Badge.d.ts
@@ -0,0 +1,7 @@
+type Props = {
+    botContainer: HTMLDivElement | undefined;
+    badgeBackgroundColor?: string;
+};
+export declare const Badge: (props: Props) => import("solid-js").JSX.Element;
+export {};
+//# sourceMappingURL=Badge.d.ts.map
